4bfdcd08e7b777721aa46e8c6e225cd64307d814
AKJ1 <kellerr@abv.bg>
2015-05-07 02:20:10 +0300

    Tuned Routing Works now.

--- services/router.php
+++ services/router.php
@@ -1,32 +1,50 @@
 <?php
 
 if (!isset($request_home)) {
-	$request_home = DS + ROOT_PATH;
+	$request_home = '/';
 }
 $request = $_SERVER['REQUEST_URI'];
 if ($request != null) {
 	//Clean up request
-	$clean_request = substr($request, strpos($request, $request_home));
+	$clean_request = substr($request, strpos($request, $request_home) + 1);
 
-	$request_components = explode(DS, $clean_request);
-
+	$request_components = array();
+	$request_components = explode('/', $clean_request);
 	if (in_array('admin', $request_components)) {
 		$admin_routing = true;
 		include_once 'controllers/admin/AdminController.php';
 		unset($request_components[array_search('admin', $request_components)]);
 		echo "im admin";
+	} else {
+		$admin_routing = false;
 	}
 	if (count($request_components) > 1) {
 		list($controller, $method) = $request_components;
 		$param = isset($request_components[2]) ? $request_components[2] : array();
 	}
 }
+echo "</br>request_home:";
+var_dump($request_home);
+echo "</br>request:";
+var_dump($request);
+echo "</br>clean request:\n";
+var_dump($clean_request);
+echo "</br>split request:\n";
+var_dump($request_components);
+echo "</br>controller:\n";
+var_dump($controller);
+echo "</br>method:\n";
+var_dump($method);
+echo "</br>param:\n";
+var_dump($param);
 
-if (isset($controller) && file_exists('controllers/' . $controller . '.php')) {
+echo 'controllers\\' . ucfirst($controller) . 'Controller' . '.php';
+
+if (isset($controller) && file_exists('controllers\\' . ucfirst($controller) . 'Controller' . '.php')) {
 	$admin_folder = $admin_routing ? 'admin/' : '';
-	include_once 'controllers' . $admin_folder . $controller . '.php';
+	include_once 'controllers\\' . $admin_folder . $controller . 'Controller' . '.php';
 
-	$controller_class = '\Controllers\\' . ucfirst($controller) . 'Controller';
+	$controller_class = 'Controllers\\' . ucfirst($controller) . 'Controller';
 	$controller_instance = new $controller_class();
 
 	if (method_exists($controller_instance, $method)) {
@@ -38,4 +56,9 @@ if (isset($controller) && file_exists('controllers/' . $controller . '.php')) {
 } else {
 	$base_controller->home();
 }
+
+function dumpVariables() {
+
+}
+
 ?>
---
+++ views/albums/home.php
@@ -0,0 +1,3 @@
+<div>
+	<img src="http://a932.phobos.apple.com/us/r1000/113/Purple2/v4/e6/e8/7b/e6e87b2b-a173-d60f-dd21-1908827ac187/mzl.zrhskomf.png" alt="">
+</div>
